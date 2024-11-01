cc163619fe412917a8c691e07e50eaede851a455
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edwindelacruz@Farmaenlace.com>
2024-10-31 21:02:15 -0500

    Fix bugs - Last build

--- .svelte-kit/output/server/manifest-full.js
+++ .svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["android-chrome-192x192.png","android-chrome-512x512.png","apple-touch-icon.png","favicon-16x16.png","favicon-32x32.png","favicon.ico","favicon.png","icono01.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.BWQ8azNq.js","app":"_app/immutable/entry/app.DSJlmdOc.js","imports":["_app/immutable/entry/start.BWQ8azNq.js","_app/immutable/chunks/entry.HkzUYOWK.js","_app/immutable/chunks/scheduler.CRfHRaee.js","_app/immutable/entry/app.DSJlmdOc.js","_app/immutable/chunks/scheduler.CRfHRaee.js","_app/immutable/chunks/index.D2DhM-K_.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
+		client: {"start":"_app/immutable/entry/start.niaAcPZU.js","app":"_app/immutable/entry/app.057e9DZT.js","imports":["_app/immutable/entry/start.niaAcPZU.js","_app/immutable/chunks/entry.BxJ1lRRx.js","_app/immutable/chunks/scheduler.CRfHRaee.js","_app/immutable/entry/app.057e9DZT.js","_app/immutable/chunks/scheduler.CRfHRaee.js","_app/immutable/chunks/index.D2DhM-K_.js"],"stylesheets":[],"fonts":[],"uses_env_dynamic_public":false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
---
+++ www/_app/immutable/entry/start.niaAcPZU.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BxJ1lRRx.js";export{t as start};
